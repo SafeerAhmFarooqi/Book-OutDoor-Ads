7c9951302ee8c591a929adb86aff3313b2b8a864
SafeerAhmFarooqi <saf_farooqi@hotmail.com>
2022-05-30 01:24:09 +0500

    Update

---
+++ resources/views/layouts/get-theme.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <!-- Primary Meta Tags -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="title" content="{{ config('app.name') }}">
+    <meta name="author" content="Safeer Ahmed Farooqi">
+    <title>{{ config('app.title') }} - {{ $headTitle ?? '' }}</title>
+    <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic:400,700,800" rel="stylesheet">
+    @yield('Styles')
+</head>
+<body>
+@yield('content')
+@yield('pageScripts')
+</body>
+</html>
