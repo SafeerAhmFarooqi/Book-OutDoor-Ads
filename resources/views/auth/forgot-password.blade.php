759d8e75bfc48d52dad05244bffc5e6e6308e7fd
SafeerAhmFarooqi <saf_farooqi@hotmail.com>
2022-08-27 00:23:21 +0500

    Password Reset And Forgot Layout Update

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -33,4 +33,206 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+@extends('layouts.led-theme')
+
+@section('content')
+<div class="site-blocks-cover overlay" style="background-image: url({{asset('assets/Led-Theme/images/hero_2.jpg')}});" data-aos="fade" data-stellar-background-ratio="0.5">
+    <div class="container">
+      <div class="row align-items-center justify-content-center text-center">
+
+        <div class="col-md-12">
+          
+          
+          <div class="row justify-content-center mb-4">
+            <div class="col-md-8 text-center">
+              <h1 class="" data-aos="fade-up">Largest LED Classifieds In  World
+              </h1>
+              <p data-aos="fade-up" data-aos-delay="100">You can buy, sell anything you want.</p>
+              <p data-aos="fade-up" data-aos-delay="100">{{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}</p>
+              
+            </div>
+          </div>
+
+          
+        </div>
+      </div>
+    </div>
+  </div>  
+
+  
+  
+  <div class="site-section" data-aos="fade">
+    <div class="container">
+      <div class="row justify-content-center mb-5">
+        <div class="col-md-7 text-center border-primary">
+          <h2 class="font-weight-light text-primary">Forgot Password</h2>
+          <p class="color-black-opacity-5">{{ __('Enter Your Email Address') }}</p>
+        </div>
+      </div>
+      <div class="row justify-content-center mb-1">
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" type="email" name="email" value="{{old('email')}}" required autofocus >
+            </div>
+            <button type="submit" class="btn btn-primary">{{ __('Email Password Reset Link') }}</button>
+          </form>
+      </div>
+    </div>
+  </div>   
+@endsection
+
+@section('Styles')
+    @parent
+
+    
+<style>
+    /*
+    # Welcome
+    --------------------------------*/
+    
+    .symbol.symbol-50px>img{width:50px;height:50px}
+    .symbol-label{display:flex;align-items:center;justify-content:center;font-weight:500;color:#3f4254;background-color:#f5f8fa;background-repeat:no-repeat;background-position:center center;background-size:cover;border-radius:.475rem}
+
+    .home-page-welcome {
+        position: relative;
+        padding: 96px 0;
+        background: url("assets/Led-Theme/images/slide1.jpg") no-repeat center;
+        background-size: cover;
+        z-index: 99;
+    }
+    
+    .home-page-welcome::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        width: 100%;
+        height: 100%;
+        background: rgba(21,21,21,.9);
+    }
+    
+    .welcome-content .entry-title {
+        position: relative;
+        padding-bottom: 24px;
+        font-size: 36px;
+        font-weight: 600;
+        color: #fff;
+    }
+    
+    .welcome-content .entry-title::before {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 64px;
+        height: 4px;
+        border-radius: 2px;
+        background: #2c35da;
+    }
+    
+    .welcome-content .entry-content {
+        font-size: 14px;
+        line-height: 2;
+        color: #b7b7b7;
+    }
+    
+    .home-page-welcome img {
+        display: block;
+        width: 100%;
+    }
+    
+    @media screen and (max-width: 992px){
+        .home-page-welcome img {
+            margin-bottom: 60px;
+        }
+    }
+    
+    /*
+    
+    /*
+    # Home Milestone
+    --------------------------------*/
+    .home-page-limestone {
+        padding: 96px 0;
+    }
+    
+    .home-page-limestone .section-heading .entry-title {
+        padding-bottom: 36px;
+        line-height: 1.6;
+    }
+    
+    .home-page-limestone .section-heading p {
+        font-size: 14px;
+        color: #595858;
+    }
+    .site-footer::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        width: 100%;
+        height: 101%;
+        background: rgba(22,22,22,.92);
+    }
+    .footeruiclass li a
+    {
+      color: #fff;
+      font-size: 15px;
+    }
+    .footeruiclass
+    {
+        padding-top: 15px;
+    }
+    </style>
+@endsection
+
+
+@section('modals')
+<div class="modal" id="showLedCalanderModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Modal Heading</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <!-- Modal body -->
+        <div class="modal-body" id="showLedCalander">
+          <!--<input type="text" name="daterange" id="demoDate" class="demo">-->
+        </div>
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('pageScripts')
+
+
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key=AIzaSyAIeDyz_v1KkoU3ZTRqK5e-9Ax1lNjSIEI"></script>
+<script type="text/javascript">
+    var searchInput = 'googleLocation';
+    
+        $(document).ready(function () {
+            var autocomplete;
+            autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+                types: ['geocode']
+               
+            });
+        
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                var near_place = autocomplete.getPlace();
+            });
+        });
+</script>
+@endsection
