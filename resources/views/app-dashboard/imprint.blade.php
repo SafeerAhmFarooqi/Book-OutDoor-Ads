31930c3c77a1237cc1e4683ef3fadd29a6dd10f1
SafeerAhmFarooqi <saf_farooqi@hotmail.com>
2022-09-10 16:52:45 +0500

    Image and imprint replace

--- resources/views/app-dashboard/imprint.blade.php
+++ resources/views/app-dashboard/imprint.blade.php
@@ -9,21 +9,25 @@
  <div class="wrapper"  >
  
     <div class="col-sm-8">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2545.520407153666!2d7.589895950856159!3d50.35684997936171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47be7caf99b3c3e7%3A0xb3ed6f6d912762a4!2sEmotions+EMS+Studio+%26+Fitness+Store!5e0!3m2!1sen!2s!4v1558317747386!5m2!1sen!2s" width="100%" height="650px" frameborder="0" style="border:0" allowfullscreen></iframe>
+        
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2545.5192477137166!2d7.590051215261451!3d50.356871601985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47be7dffca84cd55%3A0xf736dfaefe772342!2sLed-Werbefl%C3%A4chen.de!5e0!3m2!1sen!2s!4v1662810324762!5m2!1sen!2s" width="100%" height="650px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
     </div>
      
     <div class="contact-form col-sm-4">
         <h1 class="title" style="font-weight:bold;padding-bottom:20px">Impressum</h1><br>
-        <p> Heinen data systems GmbH</p><br>
-        <p>Simmern Street 86a</p><br>
-        <p> 56075 Koblenz</p><br>
+        <p> LED-Werbeflächen Marketing GmbH                                  </p><br>
+        <p>  Löhrstraße 87 a/b</p><br>
+        <p> 56068 Koblenz</p><br>
 
  
-    <p>Telefon: +49 (0)261 803090</p><br>
-    <p>Fax: +49 (0)261 85426</p><br>
+    <p>Kontakt     : +49 176 80850725 </p><br>
+    <p>Amtsgericht Koblenz               HRB-Nr : 25328</p><br>
 
-    <p>E-Mail: p.heinen@heinen-datensysteme.de</p><br>
-    <p>Website:  <a href="http://heinen-it.com/">heinen-it.com</a> </p><br> 
+    <p>Ust-ID-Nr: DE 22/650/03594  </p><br>
+    <p>Geschäftsführer :  Uwe Hochalter</p><br> 
 
          
     </div>
