db6e32cd99819063c1de1399d3c2777c9ebf412e
SafeerAhmFarooqi <saf_farooqi@hotmail.com>
2022-09-14 15:47:37 +0500

    Update

--- resources/views/app-dashboard/agb.blade.php
+++ resources/views/app-dashboard/agb.blade.php
@@ -6,42 +6,219 @@
 @section('content')
   
  
- <div class="wrapper"  >
+ <div class="container-fluid"  >
+        <h1 style="font-weight: bold;padding-top:50px !important;font-size: 22px;padding-bottom: 30px !important;">Allgemeine Geschäftsbedingungen der LED Werbeflächen Marketing GmbH</h1> 
+
+
+    <div class="col-sm-6">
+        <div class="col-sm-12">
+            <h3 style="font-weight:bold">1. Gegenstand des Unternehmens</h3><br>
+            <p style="text-align: justify;">Gegenstand des Unternehmens LED-Marketing GmbH ist die Vermarktung von Werbefläche auf
+                verschiedensten Videoflächen ohne Regionale einschränkung. Die Erfüllung erteilter und
+                angenommene Aufträge werden von dem Unternehmen im eigenem Namen und auf eigene Rechnung im
+                Auftrag des jeweiligen Werbekunden (nachfolgend »Auftraggeber« genannt) durch-geführt.</p>    
+        </div>    
+
+
+        <div class="col-sm-12"> <Br>
+            <h3 style="font-weight:bold">2. Gewährleistung</h3><br>
+            <p style="text-align: justify;">
+            2.1. Der Auftraggeber ist verpflichtet, die erteilte Werbung vor Zusendung zu prüfen. Erfolgt keine Reklamation bis zur Ausstrahlung, entfallen die Gewährleistungsansprüche des Auftraggebers. <Br> <Br>
+2.2. Bei fehlerhafter Schaltung der Werbung ist das Unternehmen LED-Werbeflächen Marketing GmbH
+nach seiner Wahl berechtigt, entweder eine Ersatzschaltung in dem Maße, in dem der Zweck der Schaltung
+beeinträchtigt wurde, oder eine Nachbesserung vorzunehmen. Erst wenn eine solche Ersatzschaltung oder
+Nachbesserung unmöglich, mindestens zweimal fehlgeschlagen ist, unzumutbar ver-zögert oder dem
+Unternehmen trotz angemessener Fristsetzung durch den Auftraggeber verweigert wurde, steht dem
+Aufraggeber wahlweise das Recht zur Rückgängigmachung des Vertrages (Wandlung) oder zur
+Herabsetzung der Vergütung (Minderung) zu. <Br> <Br>
+2.3. Technische Störungen sind vom Unternehmen innerhalb von 5 Werktagen zu beheben. Darüber hinaus
+können keine Ansprüche geltend gemacht werden. <Br>
+2.4. Sollten Fehler bei der Ausführung eines Auftrags entstehen, ist der Auftraggeber nicht berechtigt, die
+Zahlung eines anderen Auftrags zu verweigern. Eine Aufrechnung ist nur zulässig, wenn der zur Aufrechnung
+gestellte Anspruch des Auftraggebers unbestritten oder rechtskräftig festgestellt ist.
+
+
+</p>    
+        </div>  
+
+
+        <div class="col-sm-12"><br>
+            <h3 style="font-weight:bold">3. Pflichten des Auftraggebers</h3><br>
+            <p style="text-align: justify;">
+                3.1. Der Auftraggeber bestätigt mit der Auftragserteilung, dass er sämtliche erforderlichen Nutzungsrechte
+der Inhaber von Urheber-, Leistungsschutz- und sonstigen Rechten an den von ihm gestellten
+Werbeunterlagen und -texten erworben hat und frei darüber verfügen kann.<br><br>
+3.2. Der Auftraggeber stellt das Unternehmen von allen Kosten und Ansprüchen Dritter frei, die aus der Verletzung Urheber-, Wettbewerbs-, Presse-, strafrechtlicher oder sonstiger rechtlicher Bestimmungen bei dem
+Unternehmen entstehen können.<br><br>
+3.3. Der Auftraggeber ist für die rechtzeitige und technisch einwandfreie Anlieferung der Werbemittel verantwortlich. Dies beinhaltet auch den technischen Aufbau der Werbemittel gemäß den in der
+Auftragsbestätigung angegebenen jeweiligen Spezifikationen. Der Auftraggeber trägt das Risiko für die
+Übermittlung des Werbe-mittels, soweit nicht aus dem Risikobereich des Unternehmens Probleme bei der
+Übermittlung auftreten.<br><br>
+3.4. Können Werbeaufträge aus Gründen, die im Risikobereich des Auftraggebers liegen, nicht oder fehlerhaft durchgeführt werden, wird die vereinbarte Werbung dem Auftraggeber trotzdem in Rechnung gestellt.
+Trifft dem Unternehmen keinerlei Verschulden an der fehlerhaften oder Nichtausführung, so hat der Auftraggeber keine Ansprüche gegen das Unternehmen.<br><br>
+3.5. Der Auftraggeber ist nicht berechtigt, ohne ausdrückliche vorherige Zustimmung dem Unternehmen
+Rechte oder Pflichten aus dem Vertrag an Dritte abzutreten oder zu übertragen. Das Unternehmen kann
+Rechte und Pflichten aus dem Vertrag jederzeit an einen Dritten abtreten oder übertragen. Der Auftraggeber
+erteilt dazu ausdrücklich seine Zustimmung. Solange der Auftraggeber von der Übertragung nicht verständigt
+wurde, ist er berechtigt mit schuldbefreiender Wirkung an das Unternehmen zu zahlen.
+
+
+            </p>    
+        </div>   
+
+
+        <div class="col-sm-12"><br>
+            <h3 style="font-weight:bold">4. Rücktrittsrecht</h3><br>
+            <p style="text-align: justify;">
+
+            4.1. Das Unternehmen behält sich das Recht vor, auch nach Vertragsabschluss die Werbeschaltung aus
+Gründen abzulehnen, die für das Unternehmen eine Vertragsdurchführung unzumutbar machen. Dies ist
+insbesondere dann der Fall, wenn die Werbeschaltung Urheber-, Wettbewerbs-, Presse-, strafrechtliche oder
+sonstige rechtliche Bestimmungen verletzt.<br><br>
+4.2. Der Auftraggeber hat das Recht, über die Gründe der Zurückweisung informiert zu werden. Kann vom
+Auftraggeber kein neues, den Anforderungen des Unternehmens entsprechendes Werbematerial zur Verfügung gestellt werden, hat der Auftraggeber keinen Anspruch auf Rückerstattung bereits geleisteter Zahlungen.
+Trifft dem Unternehmen an der Unzumutbarkeit der Vertragsdurchführung kein Verschulden, so sind die
+vereinbarten Zahlungen zu leisten.
+
+</p>    
+        </div>  
+
+
+        <div class="col-sm-12"><br>
+            <h3 style="font-weight:bold">5. Stornierung</h3><br>
+            <p style="text-align: justify;">Die Stornierung von Aufträgen durch den Kunden ist grundsätzlich nach Vertragsabschluss nicht möglich.
+</p>    
+        </div>  
+
+
+        <div class="col-sm-12"><br>
+            <h3 style="font-weight:bold">6. Preise</h3><br>
+            <p style="text-align: justify;">6.1. Für die Verträge gelten die im Zeitpunkt des Vertragsabschlusses jeweils gültigen Preislisten des Unternehmens, sofern einzelvertraglich nicht etwas anderes vereinbart wurde.<br><br>
+6.2. Preise für Spoterstellung des Unternehmens sind grundsätzlich nicht im Preis für die Schaltungen enthalten
+</p>    
+        </div>  
+
+
+        <div class="col-sm-12"><br>
+            <h3 style="font-weight:bold">7. Geltungsbereich
+</h3><br>
+            <p style="text-align: justify;">
+            7.1. Für alle mit dem Unternehmen abzuschließenden/abgeschlossenen erstmaligen, laufenden und künftigen Geschäfte gelten ausschließlich die nachfolgenden Bedingungen. Das Unternehmen erkennt von den vorliegenden Allgemeinen Geschäftsbedingungen abweichende Bedingungen des Auftraggebers nicht an. Diese
+werden selbst dann nicht Vertragsbestandteil, wenn das Unternehmen ihnen nicht ausdrücklich widerspricht<br><br>
+
  
-    <div class="col-sm-8">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2545.520407153666!2d7.589895950856159!3d50.35684997936171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47be7caf99b3c3e7%3A0xb3ed6f6d912762a4!2sEmotions+EMS+Studio+%26+Fitness+Store!5e0!3m2!1sen!2s!4v1558317747386!5m2!1sen!2s" width="100%" height="650px" frameborder="0" style="border:0" allowfullscreen></iframe>
-    </div>
-     
-    <div class="contact-form col-sm-4">
-        <h1 class="title" style="font-weight:bold;padding-bottom:20px">Impressum</h1><br>
-        <p> LED-Werbeflächen Marketing GmbH, Löhrstraße 87a/b 56068 Koblenz</p><br>
+7.2.    Mit der Erteilung des Auftrages wird die ausschließliche Gültigkeit dieser Geschäftsbedingungen durch den Kunden anerkannt.
 
-    <p>Gesellschafter: Uwe Hochhalter, Viktor Hochhalter, Danny Hochhalter.     </p><br>
-
-    <p>Telefon: 026120069568</p><br>
-
-    <p>E-Mail: hochhalter@led-werbeflaechen.de</p><br>
-
-    <p>Umsatzsteuer-Identifikationsnummer gem. § 27a UStG: DE305892470</p><br>
-
-    <p>Inhaltlich Verantwortlicher gem. § 55 II RStV: Uwe Hochhalter (Anschrift s.o.)</p>
-
-         
-    </div>
-</div>
- 
+</p>    
+        </div>  
 
 
-        </div> 
+    </div> 
+
+
+
+
+
+
+
+
+    <div class="col-sm-6">
+        <div class="col-sm-12"><br>
+
+        <h3 style="font-weight:bold">8.   Vertragsabschluss</h3><br>
+                <p style="text-align: justify;"> 
+8.1.    Ein Vertrag zwischen dem Unternehmen und dem Auftraggeber kommt entweder durch eine schriftliche Auftragsbestätigung per Brief, Fax oder E-Mail seitens des Unternehmens oder durch Erfüllung des Auftrags seitens des Unternehmens zustande. Das Unternehmen hat das Recht, noch nicht bestätigte Aufträge auch ohne Angabe von Gründen abzulehnen.<br><br>
+8.2.    Vom Auftraggeber mündlich erteilte Aufträge und Auftragsänderungen bereits bestätigter Aufträge wer- den nur wirksam, wenn sie von dem Unternehmen schriftlich bestätigt sind.<br><br>
+8.3.    Für die Verträge gelten die im Zeitpunkt des Vertragsabschlusses jeweils gültigen Preislisten des Unter- nehmens.<br><br>
+8.4.    Die Schaltungen der Werbung wie Einschaltzeit, Platzierung und Reihenfolge werden vom Unternehmen bestimmt.<br><br>
+8.5.    Der Auftraggeber stimmt einer kostenlosen Anzeige seiner Spots auf der Website von
+LED-Werbeflächen Marketing GmbH als Referenz zu. Der Auftraggeber kann dieser Anzeige widersprechen.
+</p>
+
+         </div>
+
+        <div class="col-sm-12"><br>
+
+        <h3 style="font-weight:bold">9.   Haftung</h3><br>
+                <p style="text-align: justify;">
+9.1.    Das Unternehmen haftet nicht dafür, dass durch die Schaltung der Werbung, bestimmte Ergebnisse erzielt werden können.<br><br>
+9.2.    Für Schadensersatz, gleich aus welchem Rechtsgrund, haftet das Unternehmen nur bei Vorsatz, grober Fahrlässigkeit und Fehlen einer zugesicherten Eigenschaft. Dies gilt auch für Erfüllungs- bzw. Verrichtungs- gehilfen und gesetzliche Vertreter des Unternehmens.<br><br>
+9.3.    In allen anderen Fällen haftet das Unternehmen nur dann, wenn wesentliche Vertragspflichten (Kardi- nalspflichten) verletzt sind. Dabei ist der Schadensersatz auf den typischerweise vorhersehbaren Schaden begrenzt. Ein Ausgleich von atypischen oder nicht vorhersehbaren Schäden findet nicht statt.
+</p>
+
+         </div>
+
+        <div class="col-sm-12"><br>
+
+        <h3 style="font-weight:bold">10.  Haftungsausschluss</h3><br>
+                <p style="text-align: justify;">
+Das Unternehmen haftet nicht für die Aktualität, die inhaltliche Richtigkeit sowie für die Vollständigkeit der in ihrem Webangebot eingestellten Informationen.
+</p>
+
+         </div>
+
+        <div class="col-sm-12"><br>
+
+        <h3 style="font-weight:bold">11.  Copyright</h3><br>
+                <p>
+Das Layout, die verwendeten Grafiken und Bilder, die Sammlung von Beiträgen sowie einzelne Beiträge sind urheberrechtlich geschützt. Eine Vervielfältigung oder Verwendung ist ohne ausdrückliche Genehmigung des Autors nicht gestattet. Alle Rechte behält sich das Unternehmen vor.
+</p>
+
+         </div>
+
+        <div class="col-sm-12"><br>
+
+        <h3 style="font-weight:bold">12.  Erfüllungsort und Gerichtsstand</h3><br>
+                <p style="text-align: justify;">
+12.1.   Erfüllungsort und Gerichtsstand ist, soweit rechtlich zulässig, der Sitz des Unternehmens. Das Unterneh- men ist allerdings berechtigt, Ansprüche gegen den Auftraggeber auch vor jedem anderem Gericht geltend zu machen.<br><br>
+12.2.   Für die Vertragsabschlüsse gilt deutsches Recht.
+</p>
+
+         </div>
+
+                 <div class="col-sm-12"><br>
+
+        <h3 style="font-weight:bold">13. Sonstiges</h3><br>
+                <p style="text-align: justify;">
+
+13.1.   Änderungen oder Ergänzungen zum Vertrag bedürfen zu ihrer Rechtswirksamkeit der Schriftform. Mündliche Nebenabreden gelten nicht.<br><br>
+13.2.   Sollten einzelne Teile der vorstehenden Allgemeinen Geschäftsbedingungen unwirksam sein oder wer- den, so bleiben die übrigen Bestimmungen dieser Allgemeinen Geschäftsbedingungen voll wirksam.
+
+</p>
+
+         </div>
+
+                 <div class="col-sm-12"><br>
+
+        <h3 style="font-weight:bold">14. Zahlungen</h3><br>
+                <p style="text-align: justify;">
+
+14.1.   Rechnungen sind sofort, spätestens jedoch ab dem erstem Tag der Schaltung der Werbung und inner- halb 7 Tagen, nach ihrem Erhalt ohne jeden Abzug auf ein von dem Unternehmen angegebenes Konto zu leisten. Bei Teilnahme am Lastschriftverfahren wird innerhalb 7 Tagen nach Erstausstrahlung die Zahlung angewiesen. Folgebeiträge werden monatlich eingezogen.<br><br>
+14.2.   Ist die Erfüllung des Zahlungsanspruchs wegen einer nach Vertragsschluss eingetretenen oder bekannt gewordenen Verschlechterung der Vermögensverhältnisse des Auftraggebers gefährdet, so kann das Unter- nehmen die weitere Ausführung eines Schaltauftrags bis zur Bezahlung zurückstellen oder für weitere Wer- beschaltungen unbeschadet, entgegenstehender früherer Vereinbarung, eine Vorauszahlung oder andere Sicherheiten verlangen.<br><br>
+14.3.   Kommt der Auftraggeber mit seiner Zahlungspflicht ganz oder teilweise in Verzug, so hat er ab diesem Zeitpunkt Verzugszinsen in Höhe von jährlich 5% über dem Basiszins der EZB zu zahlen, soweit das Unter- nehmen nicht einen höheren Schaden nachweist.<br><br>
+14.4.   Das Unternehmen ist berechtigt Vorauskasse zu verlangen und erst nach Erhalt des Rechnungsbetrages Leistung zu erbringen.
+
+</p>
+
+         </div>
+
+
+
+
 
 
 
 
 
     </div> 
+ 
+ 
+
+
+ 
 
   
-  
+ </div> 
     
 @endsection
 
