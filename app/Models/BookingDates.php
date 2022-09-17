ca1d19f6058d05d4186b8138cb1ee645eef1ba5e
SafeerAhmFarooqi <saf_farooqi@hotmail.com>
2022-09-17 11:07:05 +0500

    Booking Table Deployed

---
+++ app/Models/BookingDates.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BookingDates extends Model
+{
+    use HasFactory;
+
+    protected $table = 'booking_dates';
+    protected $dates = ['bookdate'];
+    public $timestamps = true;
+
+    protected $fillable = [
+        'user_id',
+        'led_id',
+        'order_id',
+        'suborder_id',
+        'bookdate',
+    ];
+}
