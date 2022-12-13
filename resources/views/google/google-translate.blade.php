b3bcdeb51726417d9ec0fbfd09003a46fb2efd82
SafeerAhmFarooqi <saf_farooqi@hotmail.com>
2022-12-13 18:30:02 +0500

    Google Translation Fix

--- resources/views/google/google-translate.blade.php
+++ resources/views/google/google-translate.blade.php
@@ -1,14 +1,41 @@
 <div id="google_translate_element" style=""></div>
-<style>#google_translate_element,.skiptranslate{display:none;}body{top:0!important;}</style>
+<style>
+#google_translate_element,
+.skiptranslate{display:none;}
+body{top:0!important;}
+.goog-te-gadget {
+    display:none !important;
+}
+.goog-te-banner-frame.skiptranslate {
+    display: none !important;
+}
+.goog-tooltip {
+    display: none !important;
+}
+.goog-tooltip:hover {
+    display: none !important;
+}
+.goog-text-highlight {
+    background-color: transparent !important;
+    border: none !important; 
+    box-shadow: none !important;
+}
+#goog-gt-tt, .goog-te-balloon-frame{display: none !important;} 
+.goog-text-highlight { background: none !important; box-shadow: none !important;}
+.goog-logo-link{display: none !important;}
+.goog-te-gadget{height: 28px !important;  overflow: hidden;}
+</style>
 <script type="text/javascript">
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({
   	pageLanguage: 'en',
-  	includedLanguages:'en'
+  	includedLanguages:'de',
+	  layout:     google.translate.TranslateElement.InlineLayout.SIMPLE,
+	  autoDisplay:false,
   }, 'google_translate_element');
   setTimeout(function(){
   	var select = document.querySelector('select.goog-te-combo');
-  	select.value    = "en"; 
+  	select.value    = "de"; 
   	select.dispatchEvent(new Event('change'));
   },2000)
 }
